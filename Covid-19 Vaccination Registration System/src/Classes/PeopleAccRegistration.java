2c0133fae2e9f5bf24987b76ddce505a2ab859aa
lydia0423 <lydia08248@yahoo.com>
2021-11-19 09:07:36 +0800

    Update
    - change package name
    - change the appointment id to random generate number

---
+++ Covid-19 Vaccination Registration System/src/Classes/PeopleAccRegistration.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import HelperClasses.FileHandler;
+
+/**
+ *
+ * @author Eric
+ */
+public class PeopleAccRegistration extends Register{
+    
+    protected String citizen, name, icpassport, contact, address, dob;
+    
+    public PeopleAccRegistration(String email, String password, String citizen, String name, String icpassport, String contact, String address, String dob) {
+        super(email, password);
+        this.citizen = citizen;
+        this.name = name;
+        this.icpassport = icpassport;
+        this.contact = contact;
+        this.address = address;
+        this.dob = dob;
+    }
+
+    public String getCitizen() {
+        return citizen;
+    }
+       
+    public void setCitizen(String citizen) {
+        this.citizen = citizen;
+    }
+     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcpassport() {
+        return icpassport;
+    }
+
+    public void setIcpassport(String icpassport) {
+        this.icpassport = icpassport;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    //Save New Registration into File
+    public static void saveRegistration(PeopleAccRegistration register) {
+        
+        String fileName = "peoplecredentials.txt";
+
+        File myFile = FileHandler.createFilePath("Credentials", fileName);
+        try ( FileWriter fw = new FileWriter(myFile, true);  BufferedWriter bw = new BufferedWriter(fw);) {
+            bw.write(register.getEmail() + " | ");
+            bw.write(register.getPassword() + " | ");
+            bw.write(register.getCitizen() + " | ");
+            bw.write(register.getName() + " | ");
+            bw.write(register.getIcpassport() + " | ");
+            bw.write(register.getContact() + " | ");
+            bw.write(register.getAddress() + " | ");
+            bw.write(register.getDob() + "\n");
+            bw.close();
+            
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Failed to save registration. Please try again.", "Account Registration Failed", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Error occurred: " + e);
+            return;
+        }
+
+        JOptionPane.showMessageDialog(null, "Account registered successfully.", "Account Registration Success!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ Covid-19 Vaccination Registration System/src/Classes/VaccinationAppointment.java
@@ -0,0 +1,271 @@
+package Classes;
+
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+import java.io.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+import HelperClasses.FileHandler;
+import HelperClasses.FileHandler;
+
+public class VaccinationAppointment {
+
+    protected String appointmentId, patientName, patientIdentification, state, vaccinationCenter, vaccineType, registeredDate, appointmentDate, appointmentTime, healthCondition, closeContact, appointmentStatus;
+
+    public VaccinationAppointment(String appointmentId, String patientName, String patientIdentification, String state, String vaccinationCenter, String vaccineType, String registeredDate, String appointmentDate, String appointmentTime, String healthCondition, String closeContact, String appointmentStatus) {
+        this.appointmentId = appointmentId;
+        this.patientName = patientName;
+        this.patientIdentification = patientIdentification;
+        this.state = state;
+        this.vaccinationCenter = vaccinationCenter;
+        this.vaccineType = vaccineType;
+        this.registeredDate = registeredDate;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.healthCondition = healthCondition;
+        this.closeContact = closeContact;
+        this.appointmentStatus = appointmentStatus;
+    }
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientId) {
+        this.patientName = patientId;
+    }
+
+    public String getPatientIdentification() {
+        return patientIdentification;
+    }
+
+    public void setPatientIdentification(String patientIdentification) {
+        this.patientIdentification = patientIdentification;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getVaccinationCenter() {
+        return vaccinationCenter;
+    }
+
+    public void setVaccinationCenter(String vaccinationCenter) {
+        this.vaccinationCenter = vaccinationCenter;
+    }
+
+    public String getVaccineType() {
+        return vaccineType;
+    }
+
+    public void setVaccineType(String vaccineType) {
+        this.vaccineType = vaccineType;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getHealthCondition() {
+        return healthCondition;
+    }
+
+    public void setHealthCondition(String healthCondition) {
+        this.healthCondition = healthCondition;
+    }
+
+    public String getCloseContact() {
+        return closeContact;
+    }
+
+    public void setCloseContact(String closeContact) {
+        this.closeContact = closeContact;
+    }
+
+    public String getAppointmentStatus() {
+        return appointmentStatus;
+    }
+
+    public void setAppointmentStatus(String appointmentStatus) {
+        this.appointmentStatus = appointmentStatus;
+    }
+    
+    
+
+    //Generate appointment ID
+    public static String generateAppointmentId() {
+//        ArrayList<String> existingAppointmentId = new ArrayList<>();
+//        String prefix = "A0";
+//
+//        //Check if the booking id has already been used, regenerate id if in use
+//        String[] appointmentFolder = FileHandler.retrievePath("Appointment", "null").list();
+//        for (String appointmentFolderFiles : appointmentFolder) {
+//            try ( Scanner readFiles = new Scanner(appointmentFolderFiles)) {
+//                while (readFiles.hasNext()) {
+//                    String[] fileName = readFiles.nextLine().split(Pattern.quote("."));
+//                    existingAppointmentId.add(fileName[0]);
+//                }
+//            } catch (Exception e) {
+//                System.err.println("Error retrieving files: " + e);
+//            }
+//        }
+//        //Get the last used ID and increment by 1
+//        int lastIndex = existingAppointmentId.size() - 1;
+//        String idChar = existingAppointmentId.get(lastIndex);
+//        String[] idNum = idChar.split("A");
+//
+//        int lastId = Integer.valueOf(idNum[1]);//Integer.parseInt(idNum[1]);
+//        int appointmentNum = lastId + 1;
+//        if (appointmentNum < 10) {
+//            prefix = "A000";
+//        } else if (appointmentNum < 100) {
+//            prefix = "A00";
+//        }
+//        if (existingAppointmentId.isEmpty()) {
+//            appointmentNum = 1;
+//        }
+//        String appointmentId = prefix + appointmentNum;
+//
+//        for (String id : existingAppointmentId) {
+//            while (appointmentId.equals(id)) {
+//                appointmentNum++;
+//                appointmentId = prefix + appointmentNum;
+//            }
+//        }
+
+        //Generate random combination of number and alphabet for appointment Id
+        String appointmentId = "APT_" + UUID.randomUUID().toString();
+        return appointmentId;
+    }
+
+    //Retrieve all appointments made
+    public static ArrayList<VaccinationAppointment> getAllVaccinationAppointments() {
+        //Retrive from folder
+        File appointmentFolder = FileHandler.retrievePath("Appointment", "null");
+        File[] appointmentFiles = appointmentFolder.listFiles();
+        ArrayList<VaccinationAppointment> allAppointments = new ArrayList<>();
+        for (File appointmentFile : appointmentFiles) {
+            try ( Scanner readFile = new Scanner(appointmentFile)) {
+                while (readFile.hasNext()) {
+                    allAppointments.add(new VaccinationAppointment(
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine()
+                    ));
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println(e);
+            }
+        }
+        return allAppointments;
+    }
+    
+    //Retrieve specific appointment
+    public static VaccinationAppointment getVaccinationAppointment(String fileName) {
+        //Retrieve from folder
+        File appointmentFile = FileHandler.retrievePath("Appointment", fileName);
+
+        try ( Scanner readFile = new Scanner(appointmentFile)) {
+            while (readFile.hasNext()) {
+                VaccinationAppointment appointment = new VaccinationAppointment(
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine(),
+                            readFile.nextLine()
+                );
+                return appointment;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
+        return null;
+    }
+
+    //Save Appointment
+    public static void saveAppointment(VaccinationAppointment appointment) {
+        String fileName = appointment.appointmentId + ".txt";
+
+        File myFile = FileHandler.createFilePath("Appointment", fileName);
+        try ( FileWriter fw = new FileWriter(myFile);  BufferedWriter bw = new BufferedWriter(fw);) {
+            bw.write(appointment.getAppointmentId());
+            bw.newLine();
+            bw.write(appointment.getPatientName());
+            bw.newLine();
+            bw.write(appointment.getPatientIdentification());
+            bw.newLine();
+            bw.write(appointment.getState());
+            bw.newLine();
+            bw.write(appointment.getVaccinationCenter());
+            bw.newLine();
+            bw.write(appointment.getVaccineType());
+            bw.newLine();
+            bw.write(appointment.getRegisteredDate());
+            bw.newLine();
+            bw.write(appointment.getAppointmentDate());
+            bw.newLine();
+            bw.write(appointment.getAppointmentTime());
+            bw.newLine();
+            bw.write(appointment.getHealthCondition());
+            bw.newLine();
+            bw.write(appointment.getCloseContact());
+            bw.newLine();
+            bw.write(appointment.getAppointmentStatus());
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Failed to save appointment. Please try again.", "Register Vaccination Appointment Failed", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Error occurred: " + e);
+        }
+
+        JOptionPane.showMessageDialog(null, "Vaccination Appointment successfully saved.", "Register Vaccination Appointment Success!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
